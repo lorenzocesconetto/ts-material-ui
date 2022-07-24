edd8d9f3ae4fcab96f495bd53c6017212cd3c816
Lorenzo C <lorenzo_cesconeto@hotmail.com>
2022-07-23 21:56:44 -0300

    feat: add react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+
 export const App = () => {
-  return <div className="App">Teste</div>;
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 };
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export {};
+import { Routes, Route, Navigate } from "react-router-dom";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<p>Testing router</p>} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
+};
