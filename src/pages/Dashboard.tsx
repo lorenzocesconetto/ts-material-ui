6b87aba22c86f29f2cb41e7903d9a28819afd8cd
Lorenzo C <lorenzo_cesconeto@hotmail.com>
2022-07-24 20:06:13 -0300

    feature: detail toolbar: skeleton + responsive

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,10 +1,11 @@
 import { Base } from "../shared/layouts";
 import { DetailToolbar } from "../shared/components";
+import { Typography } from "@mui/material";
 
 const Dashboard = () => {
   return (
     <Base title="Dashboard" toolbar={<DetailToolbar />}>
-      Test
+      <Typography>Dashboard content</Typography>
     </Base>
   );
 };
--- src/shared/components/DetailToolbar.tsx
+++ src/shared/components/DetailToolbar.tsx
@@ -1,4 +1,16 @@
-import { Box, Button, Divider, Paper, useTheme } from "@mui/material";
+import {
+  Box,
+  Button,
+  Divider,
+  Paper,
+  Skeleton,
+  Typography,
+  useMediaQuery,
+  useTheme,
+  Theme,
+  Tooltip,
+} from "@mui/material";
+import { ButtonProps } from "@mui/material/Button";
 import {
   Add as AddIcon,
   Save as SaveIcon,
@@ -6,8 +18,121 @@ import {
   ArrowBack as ArrowBackIcon,
 } from "@mui/icons-material";
 
-const DetailToolbar = () => {
+interface IDetailToolbarProps {
+  showButtonSave?: boolean;
+  showButtonSaveAndBack?: boolean;
+  showButtonDelete?: boolean;
+  showButtonNew?: boolean;
+  showButtonBack?: boolean;
+
+  onClickButtonSave?(): void;
+  onClickButtonSaveAndBack?(): void;
+  onClickButtonDelete?(): void;
+  onClickButtonNew?(): void;
+  onClickButtonBack?(): void;
+
+  isLoadingButtonSave?: boolean;
+  isLoadingButtonSaveAndBack?: boolean;
+  isLoadingButtonDelete?: boolean;
+  isLoadingButtonNew?: boolean;
+  isLoadingButtonBack?: boolean;
+}
+
+const CustomButton = ({
+  text,
+  startIcon,
+  ...props
+}: ButtonProps & { text: string }) => {
+  const isScreenXs = useMediaQuery((theme: Theme) =>
+    theme.breakpoints.only("xs")
+  );
+
+  const button = (
+    <Button
+      disableElevation
+      variant="outlined"
+      startIcon={isScreenXs ? null : startIcon}
+      {...props}
+    >
+      {isScreenXs && startIcon}
+      <Typography
+        variant="button"
+        noWrap
+        color={
+          props.variant && props.variant !== "outlined" ? undefined : "primary"
+        }
+      >
+        {!isScreenXs && text}
+      </Typography>
+    </Button>
+  );
+
+  return isScreenXs ? <Tooltip title={text}>{button}</Tooltip> : button;
+};
+
+const DetailToolbar = ({
+  showButtonSave = true,
+  showButtonSaveAndBack = true,
+  showButtonDelete = true,
+  showButtonNew = true,
+  showButtonBack = true,
+
+  onClickButtonSave,
+  onClickButtonSaveAndBack,
+  onClickButtonDelete,
+  onClickButtonNew,
+  onClickButtonBack,
+
+  isLoadingButtonSave = false,
+  isLoadingButtonSaveAndBack = false,
+  isLoadingButtonDelete = false,
+  isLoadingButtonNew = false,
+  isLoadingButtonBack = false,
+}: IDetailToolbarProps) => {
   const theme = useTheme();
+  const isScreenMdDown = useMediaQuery(theme.breakpoints.down("md"));
+
+  const buttonSave = (
+    <CustomButton
+      text="Save"
+      variant="contained"
+      onClick={onClickButtonSave}
+      startIcon={<SaveIcon />}
+    />
+  );
+
+  const buttonSaveAndBack = (
+    <CustomButton
+      text="Save and back"
+      onClick={onClickButtonSaveAndBack}
+      startIcon={<SaveIcon />}
+    />
+  );
+
+  const buttonDelete = (
+    <CustomButton
+      text="Delete"
+      onClick={onClickButtonDelete}
+      startIcon={<DeleteIcon />}
+    />
+  );
+
+  const buttonNew = (
+    <CustomButton
+      text="New"
+      onClick={onClickButtonNew}
+      startIcon={<AddIcon />}
+    />
+  );
+
+  const buttonBack = (
+    <CustomButton
+      text="Back"
+      onClick={onClickButtonBack}
+      startIcon={<ArrowBackIcon />}
+    />
+  );
+
   return (
     <Box
       component={Paper}
@@ -19,24 +144,56 @@ const DetailToolbar = () => {
       height={theme.spacing(5)}
       alignItems="center"
     >
-      <Button disableElevation variant="contained" startIcon={<SaveIcon />}>
-        Save
-      </Button>
-      <Button disableElevation variant="outlined" startIcon={<SaveIcon />}>
-        Save and back
-      </Button>
-      <Button disableElevation variant="outlined" startIcon={<DeleteIcon />}>
-        Delete
-      </Button>
-      <Button disableElevation variant="outlined" startIcon={<AddIcon />}>
-        New
-      </Button>
+      {showButtonSave &&
+        (isLoadingButtonSave ? (
+          <Typography variant="h3">
+            <Skeleton>{buttonSave}</Skeleton>
+          </Typography>
+        ) : (
+          buttonSave
+        ))}
+
+      {showButtonSaveAndBack &&
+        !isScreenMdDown &&
+        (isLoadingButtonSaveAndBack ? (
+          <Typography variant="h3">
+            <Skeleton>{buttonSaveAndBack}</Skeleton>
+          </Typography>
+        ) : (
+          buttonSaveAndBack
+        ))}
+
+      {showButtonDelete &&
+        (isLoadingButtonDelete ? (
+          <Typography variant="h3">
+            <Skeleton>{buttonDelete}</Skeleton>
+          </Typography>
+        ) : (
+          buttonDelete
+        ))}
+
+      {showButtonNew &&
+        !isScreenMdDown &&
+        (isLoadingButtonNew ? (
+          <Typography variant="h3">
+            <Skeleton>{buttonNew}</Skeleton>
+          </Typography>
+        ) : (
+          buttonNew
+        ))}
+
       <Box height="100%" display="flex" flex={1} justifyContent="end">
-        <Divider orientation="vertical" />
+        {showButtonBack && <Divider orientation="vertical" />}
       </Box>
-      <Button disableElevation variant="outlined" startIcon={<ArrowBackIcon />}>
-        Back
-      </Button>
+
+      {showButtonBack &&
+        (isLoadingButtonBack ? (
+          <Typography variant="h3">
+            <Skeleton>{buttonBack}</Skeleton>
+          </Typography>
+        ) : (
+          buttonBack
+        ))}
     </Box>
   );
 };
