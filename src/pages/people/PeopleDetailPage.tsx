d8800793e6bb1cceab7b54ee5da81b1323111244
Lorenzo C <lorenzo_cesconeto@hotmail.com>
2022-07-28 16:03:59 -0300

    feature: handle delete

--- src/pages/people/PeopleDetailPage.tsx
+++ src/pages/people/PeopleDetailPage.tsx
@@ -1,23 +1,56 @@
-import { Typography } from "@mui/material";
+import { LinearProgress, Typography } from "@mui/material";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { DetailToolbar } from "../../components";
 import { Base } from "../../layouts";
+import { PeopleService } from "../../services/api/people";
 
 const PeopleDetailPage = () => {
   const { id = "new" } = useParams<"id">();
   const navigate = useNavigate();
   const isEdit = id !== "new";
+  const [title, setTile] = useState(isEdit ? "" : "New person");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      if (isEdit) {
+        setIsLoading(true);
+        try {
+          const data = await PeopleService.getById(parseInt(id));
+          setTile(data.name);
+        } catch (err) {
+          console.error(err);
+          alert((err as { message: string }).message);
+          navigate("/people");
+        } finally {
+          setIsLoading(false);
+        }
+      }
+    })();
+  }, [id]);
+
+  const handleDelete = async (id: number) => {
+    if (!confirm("Are you sure you'd like to delete this record?")) return;
+    try {
+      await PeopleService.deleteById(id);
+      alert("Successfully deleted");
+      navigate("/people");
+    } catch (err) {
+      alert(err);
+    }
+  };
 
   return (
     <Base
-      title="Person detail"
+      title={title}
       toolbar={
         <DetailToolbar
           showButtonDelete={isEdit}
           showButtonNew={isEdit}
           onClickButtonSave={() => null}
           onClickButtonSaveAndBack={() => null}
-          onClickButtonDelete={() => null}
+          onClickButtonDelete={() => handleDelete(parseInt(id))}
           onClickButtonNew={() => navigate("/people/new")}
           onClickButtonBack={() => navigate("/people")}
         />
@@ -26,6 +59,7 @@ const PeopleDetailPage = () => {
       <Typography>
         Detail for user: <b>{id}</b>
       </Typography>
+      {isLoading && <LinearProgress />}
     </Base>
   );
 };
